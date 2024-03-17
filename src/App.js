e493b8feda7f668b858200eb2d1b53dba6819876
veronika <nika.kurilenko2016@gmail.com>
2024-03-17 12:17:57 +0300

    react-day-picker was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Calendar from './components/Calendar';
 // import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <Calendar/>
     </div>
   );
 }
---
+++ src/components/Calendar.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import {DayPicker} from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+function Calendar() {
+
+
+    return (
+        <div>
+        <h2>Calendar with notes</h2>
+        <DayPicker
+        
+        />
+        </div>
+    );
+}
+
+export default Calendar;
